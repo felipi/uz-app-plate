2413fd9a6476757c2d68c8ac4bcdbdf5023c4012
Uzzye <uzzye@MacBook-Pro-de-Usuario.local>
2017-09-06 10:43:12 -0300

    Atualizando para Onsen2

--- www/js/app.js
+++ www/js/app.js
@@ -1,13 +1,43 @@
 var API_URL = "";
 var METH_URL = "";
 var iTimeout = 600;
-var uzzyeApp = ons.bootstrap('uzzyeApp', ['onsen', 'ngToast', 'ngStorage', 'ui.bootstrap']);
+document.addEventListener("deviceready", onDeviceReady, false);
 var AppController = (function () {
-    function AppController($scope) {
+    function AppController($scope, $localStorage) {
         this.$scope = $scope;
+        this.$localStorage = $localStorage;
+        this.$scope.$ctrl = this;
+        AppController.instance = this;
     }
+    AppController.$inject = ['$scope', '$localStorage'];
     return AppController;
 }());
-uzzyeApp.controller('appController', AppController);
-uzzyeApp.controller('menuController', MenuController);
+var uzzyeApp = angular.module('uzzyeApp', ['onsen', 'ngToast', 'ngStorage', 'ui.bootstrap'])
+    .config(["ngToastProvider", function (ngToast) {
+        ngToast.configure({
+            dismissOnTimeout: false,
+            dismissButton: true,
+            animation: "slide",
+            verticalPosition: "bottom",
+            maxNumber: 4
+        });
+    }])
+    .run(["$localStorage", function ($localStorage) {
+        console.log("RUN");
+    }]);
+function onDeviceReady() {
+    console.log("Ready please");
+    window.setTimeout(function () {
+        uzzyeApp.controller('appController', AppController);
+        uzzyeApp.controller('menuController', MenuController);
+        ons.ready(function () {
+            ons.disableAutoStyling();
+            ons.platform.select('ios');
+            console.log("Ons ready");
+        });
+        console.log(ons.isReady());
+        angular.bootstrap(document, ["uzzyeApp"]);
+        console.log("bootstrapped");
+    }, 500);
+}
 //# sourceMappingURL=app.js.map
--- www/js/app.ts
+++ www/js/app.ts
@@ -3,14 +3,48 @@ var API_URL = "";
 var METH_URL = "";
 //*/
 var iTimeout = 600; // TEMPO UM POUCO ACIMA DAS TRANSIÇÕES
-var uzzyeApp = ons.bootstrap('uzzyeApp',['onsen', 'ngToast', 'ngStorage', 'ui.bootstrap']);
+document.addEventListener("deviceready", onDeviceReady, false);
 
 class AppController {
-    
+    static $inject = ['$scope', '$localStorage']; 
+    static instance;
     constructor(
-        private $scope: ng.IScope
-    )  {}
+        public $scope: any,
+        public $localStorage: any
+    )  {
+        this.$scope.$ctrl = this;
+        AppController.instance = this;
+    }
 }
 
-uzzyeApp.controller('appController', AppController);
-uzzyeApp.controller('menuController', MenuController);
+var uzzyeApp = angular.module('uzzyeApp',['onsen', 'ngToast', 'ngStorage', 'ui.bootstrap'])
+    .config(["ngToastProvider", function(ngToast){
+        ngToast.configure({
+            dismissOnTimeout: false,
+            dismissButton: true,
+            animation: "slide",
+            verticalPosition: "bottom",
+            maxNumber: 4
+        });
+    }])
+    .run(["$localStorage", function($localStorage) {
+        console.log("RUN");
+    }]);
+
+function onDeviceReady(){
+    console.log("Ready please");
+    window.setTimeout(function(){
+        uzzyeApp.controller('appController', AppController);
+        uzzyeApp.controller('menuController', MenuController);
+
+        ons.ready( function() {
+            ons.disableAutoStyling();
+            ons.platform.select('ios');
+            console.log("Ons ready");
+        });
+
+        console.log(ons.isReady());
+        angular.bootstrap(document, ["uzzyeApp"]);
+        console.log("bootstrapped");
+    }, 500);
+}
